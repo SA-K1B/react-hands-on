fa51c4d52239f816add0aed039c43498d7a5539b
SA-K1B <tareqshakib789@gmail.com>
2025-02-15 12:12:10 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import the message component in App.tsx
+import Message from './message';
+// use the message component in the App component
+function App() {
+  return (
+    <div>
+      <Message />
+    </div>
+  );
+}
+// export the App component
+export default App;
---
+++ src/message.tsx
@@ -0,0 +1,5 @@
+function Message() {
+  return <div>Hello World!</div>;
+}
+// just write the code to export the message component
+export default Message;
